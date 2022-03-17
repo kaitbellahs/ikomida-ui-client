8cc5d7396db06f07d8291299328797560a122568
khalid Ait Bellahs <kaitbellahs@gmail.com>
2022-03-17 16:20:01 -0300

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from './App.svelte';
 import { Network } from '@tian/components';
 
 // Network.createInstance("http://10.0.0.102:1988", "ProjX", "client");
-Network.createInstance("http://10.0.0.102:1988", "projx.test-6", "client");
+Network.createInstance("http://192.168.1.104:1988", "brcomdeluxeappkhalid-restaurant", "client");
 // Network.createInstance("http://192.168.0.104:1988", "ProjX", "client");
 
 const app = new App({
