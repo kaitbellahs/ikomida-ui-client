667ece74583033ba694c53fc969abebcdb64dabd
khalid ait bellahs <kaitbellahs@gmail.com>
2022-08-23 18:12:08 -0300

    Update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import App from './App.svelte';
 import { Network } from '@ikomida/components';
+import Routes from './stores/Routes'
 
 let isProd = false
+let appVersion = '${{version}}$'
+let agent = "client"
 try {
     isProd = (isProduction !== undefined && isProduction)
 } catch (error) {
 }
+const url = isProd ? "https://api.ikomida.com" : "http://192.168.1.200"
 
-Network.createInstance(isProd ? "https://api.ikomida.com" : "http://192.168.1.200", "${{iKomidaId}}$", "client", "6LebYzshAAAAAIXhka3WrAjus5tDXtefR1QefVZS");
+Stores.Cache.createInstance()
+Network.createInstance(url, "${{iKomidaId}}$", agent, "6LebYzshAAAAAIXhka3WrAjus5tDXtefR1QefVZS", appVersion);
+Stores.Auth.createInstance(agent)
+Stores.MenuHamburger.createInstance()
+Stores.Menu.createInstance()
+Stores.Title.createInstance()
+Stores.PushNotificationToken.createInstance()
+Stores.Navigation.createInstance(Routes);
 
 const app = new App({
     target: document.body
