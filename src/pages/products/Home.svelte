ec751d86d0137c8d1ee089d3a51752278ff4992a
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-12-29 14:51:11 -0300

    Add products header categories

--- src/pages/products/Home.svelte
+++ src/pages/products/Home.svelte
@@ -6,10 +6,11 @@
   import { Views, Utils, Stores, Types } from '@ikomida/shared-frontend'
   import { GetSettings } from '../../network/User'
   import { Settings } from '../../stores/Setup'
-  import { onMount } from 'svelte'
+  import { onMount, tick } from 'svelte'
   import { Cart } from '../../stores/Cart'
   import { Writable } from 'svelte/store'
   import { Classes } from '@ikomida/shared-types'
+  import { slide } from 'svelte/transition'
 
   const cart: Cart = Cart.instance
   const Layout: Writable<Classes.CLayout | undefined> = Stores.Layout.instance?.store
@@ -20,6 +21,8 @@
   let orderType: Types.Types.TOrderType | undefined | null = null
   let working = false
   let Store: IStore
+  let mainScroll = Stores.MainScroll.createInstance().store
+  let scrollTo = 0
 
   $: if (orderType !== null) {
     OrderType.set(orderType)
@@ -35,11 +38,20 @@
       working = true
       Stores.Loading.instance.start()
       resetTimeout()
+      itemsList = []
+      await tick()
       await cart.updateType()
       await cart.products()
       categoriesAndProducts = await all()
       Stores.Loading.instance.stop()
       working = false
+    }
+  }
+
+  function slideToCategory(category: Types.Classes.CCategoryProducts) {
+    if (category.id) {
+      const node = document.getElementById(category.id)
+      scrollTo = (node?.offsetTop ?? 0) - 64
     }
   }
 
@@ -88,10 +100,22 @@
 <Views.Scroll
   tag="content"
   {itemsList}
+  bind:scrollTo
   animationIn={$Layout?.product.animation.in}
   animationOut={$Layout?.product.animation.out}
   backgroundImage={$Layout?.backgroundImage}
+  showHeader={($mainScroll?.top ?? 0) > ($mainScroll?.offsetHeight ?? 0) / 4}
 >
+  <categories
+    style="--buttonBackground:{$Layout?.button.background};--buttonColor:{$Layout?.button.color};"
+    slot="header"
+    class="backgroundCustomColor"
+    transition:slide
+  >
+    {#each categoriesAndProducts ?? [] as category}
+      <category class="backgroundCustomColor" on:click={() => slideToCategory(category)}>{category.title}</category>
+    {/each}
+  </categories>
   <div class="shadow filters backgroundCustomColor">
     <h3 class="preparationTitle">Tempo de preparação do pedido</h3>
     {#if ($Settings?.preparation?.min ?? 0) > 0}
@@ -121,13 +145,17 @@
       />
     {/if}
   {:else if ($Settings.orderTypes?.length ?? 0) === 0}
-    <h2 class="background">{userInfo?.name ? `Olá ${userInfo?.name}, tudo bem?` : 'Bem vindo visitante'}</h2>
+    <h2 class="backgroundCustomColor background ">
+      {userInfo?.name ? `Olá ${userInfo?.name}, tudo bem?` : 'Bem vindo visitante'}
+    </h2>
     <Views.Divider />
     <Views.CentredMessage
       text="Por enquanto estamos cadastrando os produtos e preparando nosso estabelecimento digital, volte mais tarde e confira as novidades que estamos preparando para você!"
     />
   {:else}
-    <h2 class="background">{userInfo?.name ? `Olá ${userInfo?.name}, tudo bem?` : 'Bem vindo visitante'}</h2>
+    <h2 class="backgroundCustomColor background">
+      {userInfo?.name ? `Olá ${userInfo?.name}, tudo bem?` : 'Bem vindo visitante'}
+    </h2>
     <Views.Divider />
     <Views.CentredMessage
       text="Pra começarmos precisa escolher o tipo do seu pedido para podermos lhe exibir os produtos certos!"
@@ -136,6 +164,27 @@
 </Views.Scroll>
 
 <style>
+  categories {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    flex-direction: row;
+    z-index: 999;
+    place-items: stretch;
+    overflow-y: scroll;
+    padding: 8pt;
+    border-radius: 0;
+  }
+  categories > category {
+    border-radius: 4pt;
+    padding: 4pt 8pt;
+    white-space: nowrap;
+    margin: 0 4pt;
+    background-color: var(--buttonBackground);
+    color: var(--buttonColor);
+  }
   .mainPicture > :global(img) {
     object-fit: contain;
     width: 100%;
